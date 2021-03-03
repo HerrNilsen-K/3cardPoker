39f6f26453cd8f61430eff91b7570f5b27628d7f
Karl <typnilsengmail.com>
2021-03-03 15:21:45 +0100

    The randomNumber generator works out of the blue for no reason

--- src/logic/cardShuffler.cpp
+++ src/logic/cardShuffler.cpp
@@ -3,13 +3,36 @@
 //
 
 #include <iostream>
+#include <array>
 #include "cardShuffler.h"
 
 hand cardShuffler::randomCards(size_t number) {
-    hand result {};
-    //result.cType = static_cast<cardType>(m_rNum.randomNumber(static_cast<short>(cardType::BEGIN), static_cast<short>(cardType::END)));
-    result.cType = (cardType)m_rNum.randomNumber(0, 3);
-    std::cout << (short)result.cType << std::endl;
+    hand result{};
+    result.cType = (cardType) m_rNum.randomNumber(0, 3);
+    /*
+    std::array<int, 4> count{};
+    for (int i = 0; i < 4000; ++i) {
+        result.cType = (cardType) m_rNum.randomNumber(0, 3);
+        switch ((short) result.cType) {
+            case 0:
+                count[0]++;
+                break;
+            case 1:
+                count[1]++;
+                break;
+            case 2:
+                count[2]++;
+                break;
+            case 3:
+                count[3]++;
+                break;
+        }
+    }
+    std::cout << "Count at 0: " << count[0] << std::endl;
+    std::cout << "Count at 1: " << count[1] << std::endl;
+    std::cout << "Count at 2: " << count[2] << std::endl;
+    std::cout << "Count at 3: " << count[3] << std::endl << std::endl;
+     */
     return result;
 }
 
